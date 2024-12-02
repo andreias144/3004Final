6873186cd696c91b06143b414d61dd70b47ed063
Natasha <natasha@lifenotes.ca>
2024-12-02 11:34:27 -0500

    minor bug fixes, removed unused files

--- profile.cpp
+++ profile.cpp
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include "profile.h"
 =======
 #include "Profile.h"
 >>>>>>> d456991 (basic structure implmented)
+=======
+#include "profile.h"
+>>>>>>> 5d28de2 (minor bug fixes, removed unused files)
 #include <QDateTime>
 #include <QRandomGenerator>
 
